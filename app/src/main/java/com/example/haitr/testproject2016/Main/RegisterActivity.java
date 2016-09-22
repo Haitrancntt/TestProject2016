368f1d5764e6404911e9aa438f7dbdd7797b091c
Haitrancntt <haitran2402@gmail.com>
2016-09-22 15:46:19 +0700

    Initial commit

---
+++ app/src/main/java/com/example/haitr/testproject2016/Main/RegisterActivity.java
@@ -0,0 +1,61 @@
+package com.example.haitr.testproject2016.Main;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.haitr.testproject2016.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+    private EditText edregister_Username, edregister_Pass;
+    private Button btnRegister;
+    private FirebaseAuth mAuth;
+    private String sName, sPass;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        MapXml();
+    }
+
+    private void MapXml() {
+        edregister_Pass = (EditText) findViewById(R.id.edit_registerPass);
+        edregister_Username = (EditText) findViewById(R.id.edit_registerUsername);
+        btnRegister = (Button) findViewById(R.id.button_register);
+    }
+
+    private void onRegister() {
+
+        sName = edregister_Username.getText().toString();
+        sPass = edregister_Pass.getText().toString();
+        mAuth.createUserWithEmailAndPassword(sName, sPass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isComplete()) {
+                    Toast.makeText(RegisterActivity.this, "Success", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(RegisterActivity.this, "Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    public void btnRegister_Click(View view) {
+        if (sName != null && sPass != null) {
+            onRegister();
+        } else {
+            Toast.makeText(RegisterActivity.this, "Please fill all", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+}
